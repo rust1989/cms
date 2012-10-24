e733f745bc7ed587138b1468f2596ba80307c911
rust1989 <960766435@qq.com>
2012-10-24 15:40:22 +0800

    17commit

--- myweb.sql
+++ myweb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- 主机: localhost
--- 生成日期: 2012 年 10 月 22 日 09:49
+-- 生成日期: 2012 年 10 月 24 日 07:39
 -- 服务器版本: 5.0.45
 -- PHP 版本: 5.2.5
 
@@ -25,6 +25,7 @@ CREATE TABLE `my_admin` (
   `pwd` varchar(50) NOT NULL,
   `logtime` varchar(255) NOT NULL,
   `logip` varchar(255) NOT NULL,
+  `langid` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
@@ -32,9 +33,31 @@ CREATE TABLE `my_admin` (
 -- 导出表中的数据 `my_admin`
 --
 
-INSERT INTO `my_admin` (`id`, `user`, `pwd`, `logtime`, `logip`) VALUES
-(1, '123', 'e10adc3949ba59abbe56e057f20f883e', '1350487970', ''),
-(2, 'admin', 'c6cf3c7e985888502b5b72917d8de3b8', '1350870428', '127.0.0.1');
+INSERT INTO `my_admin` (`id`, `user`, `pwd`, `logtime`, `logip`, `langid`) VALUES
+(1, '123', 'e10adc3949ba59abbe56e057f20f883e', '1350487970', '', 0),
+(2, 'admin', 'c6cf3c7e985888502b5b72917d8de3b8', '1351047218', '127.0.0.1', 0);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `my_article`
+--
+
+CREATE TABLE `my_article` (
+  `id` int(11) NOT NULL auto_increment,
+  `cid` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `content` text NOT NULL,
+  `date` varchar(255) NOT NULL,
+  `author` varchar(50) NOT NULL,
+  `langid` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+--
+-- 导出表中的数据 `my_article`
+--
+
 
 -- --------------------------------------------------------
 
@@ -46,7 +69,11 @@ CREATE TABLE `my_attachment` (
   `id` int(11) NOT NULL auto_increment,
   `cid` int(11) NOT NULL,
   `picpath` varchar(255) NOT NULL,
+  `spicpath` varchar(255) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `langid` int(11) NOT NULL,
+  `width` varchar(50) NOT NULL,
   PRIMARY KEY  (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
@@ -92,7 +119,7 @@ CREATE TABLE `my_category` (
   `show` int(11) NOT NULL default '0',
   `position` varchar(50) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=11 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=16 ;
 
 --
 -- 导出表中的数据 `my_category`
@@ -102,10 +129,36 @@ INSERT INTO `my_category` (`id`, `pid`, `title`, `action`, `module`, `mid`, `sho
 (1, 0, '角色管理', '', 'Member', 1, 0, ''),
 (2, 0, '网站设置', '', 'Setting', 0, 0, ''),
 (3, 0, '网站栏目', '', 'Channel', 0, 0, ''),
-(4, 0, '栏目类型', '', 'Module', 0, 0, ''),
-(5, 0, '首页广告', '', 'Setting', 0, 0, ''),
+(4, 3, '栏目类型', 'Module', 'Channel', 0, 0, ''),
 (6, 1, '管理员设置', 'Manager', 'Member', 2, 1, '0,1'),
-(10, 1, '会员管理', 'User', 'Member', 1, 1, '0,1');
+(11, 0, '产品管理', '', 'Product', 3, 1, '0,1'),
+(10, 1, '会员管理', 'User', 'Member', 1, 1, '0,1'),
+(14, 0, '图集管理', '', 'Img', 4, 1, '0,1'),
+(13, 11, '产品分类', 'Procates', 'Product', 3, 1, '0,1'),
+(15, 14, '图集分类', 'Procates', 'Img', 4, 1, '0,1');
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `my_img`
+--
+
+CREATE TABLE `my_img` (
+  `id` int(11) NOT NULL auto_increment,
+  `tid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `picid` varchar(50) NOT NULL,
+  `langid` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 导出表中的数据 `my_img`
+--
+
+INSERT INTO `my_img` (`id`, `tid`, `name`, `date`, `picid`, `langid`) VALUES
+(1, 1, '111111', '1351062882', '', 1);
 
 -- --------------------------------------------------------
 
@@ -149,33 +202,34 @@ INSERT INTO `my_member_role` (`tid`, `name`, `permission`) VALUES
 -- --------------------------------------------------------
 
 --
--- 表的结构 `my_module`
+-- 表的结构 `my_model`
 --
 
-CREATE TABLE `my_module` (
+CREATE TABLE `my_model` (
   `mid` int(11) NOT NULL auto_increment,
   `name` varchar(255) NOT NULL,
   `module` varchar(50) NOT NULL,
+  `database` varchar(50) NOT NULL,
   PRIMARY KEY  (`mid`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
--- 导出表中的数据 `my_module`
+-- 导出表中的数据 `my_model`
 --
 
-INSERT INTO `my_module` (`mid`, `name`, `module`) VALUES
-(1, '单页', ''),
-(2, '文章列表', ''),
-(3, '产品列表', ''),
-(4, '图片列表', '');
+INSERT INTO `my_model` (`mid`, `name`, `module`, `database`) VALUES
+(1, '单页', '', 'page'),
+(2, '文章列表', '', 'article'),
+(3, '产品列表', '', 'product'),
+(4, '图片列表', '', 'img');
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `my_module_field`
+-- 表的结构 `my_model_field`
 --
 
-CREATE TABLE `my_module_field` (
+CREATE TABLE `my_model_field` (
   `id` int(11) NOT NULL auto_increment,
   `pid` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
@@ -184,9 +238,59 @@ CREATE TABLE `my_module_field` (
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
 --
--- 导出表中的数据 `my_module_field`
+-- 导出表中的数据 `my_model_field`
 --
 
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `my_procates`
+--
+
+CREATE TABLE `my_procates` (
+  `id` int(11) NOT NULL auto_increment,
+  `pid` int(11) NOT NULL,
+  `cid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `picpath` varchar(255) NOT NULL,
+  `langid` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+--
+-- 导出表中的数据 `my_procates`
+--
+
+INSERT INTO `my_procates` (`id`, `pid`, `cid`, `name`, `picpath`, `langid`) VALUES
+(1, 0, 14, '图片一', '', 1),
+(2, 0, 11, '手机', '', 1),
+(3, 2, 11, '充电器', '', 1);
+
+-- --------------------------------------------------------
+
+--
+-- 表的结构 `my_product`
+--
+
+CREATE TABLE `my_product` (
+  `id` int(11) NOT NULL auto_increment,
+  `tid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `date` varchar(50) NOT NULL,
+  `content` text NOT NULL,
+  `picid` varchar(50) NOT NULL,
+  `price` varchar(50) NOT NULL,
+  `langid` int(11) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- 导出表中的数据 `my_product`
+--
+
+INSERT INTO `my_product` (`id`, `tid`, `name`, `date`, `content`, `picid`, `price`, `langid`) VALUES
+(1, 2, '2222', '1351062937', '', '', '33333', 1);
 
 -- --------------------------------------------------------
 
